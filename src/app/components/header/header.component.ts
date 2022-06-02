db3a34e1ba0fc69f3841775875f4ef8c467fcfc5
dbentler <db3857@nyu.edu>
2022-06-02 14:25:28 -0400

    Removed Add Button from "About" page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Task Tracker';
+  title: string = 'Angular Task Tracker';
   showAddTask: boolean = false;
   subscription: Subscription;
 
-  constructor(private uiService:UiService) { 
+  constructor(private uiService:UiService, private router:Router) { 
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
   }
 
@@ -21,5 +22,9 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
